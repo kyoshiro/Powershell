3e0ee6c906c57e1f1dbbdd8aa12fcbebbbb89dc0
Roman Asendorf <r.asendorf@neusta.de>
2020-04-21 18:31:40 +0200

    functions now take parameter; added all gitpromptsettings; cleaned up code

--- Microsoft.VSCode_Profile.ps1
+++ Microsoft.VSCode_Profile.ps1
@@ -1,56 +1,58 @@
-# Set my prompt
-#function Prompt {
-#    $env:USERNAME + "@" + $env:COMPUTERNAME + " " + (Get-Location) + "> "
-#}
+# Personal VSCode PowerShell Profile
+# Author: Roman Asendorf
+# 
+# See Posh-Git documentation at: 
+# https://github.com/dahlbyk/posh-git
 
+# Directory shortcut functions
 function cd_git {
     Set-Location -Path $env:HOMEPATH/Develop/git
 }
-
 function cd_gitlab {
     Set-Location -Path $env:HOMEPATH/Develop/gitlab
 }
-
 function cd_vagrant {
     Set-Location -Path $env:HOMEPATH/Develop/gitlab/NIS/vagrant
 }
-
 function cd_p_terraform {
     Set-Location -Path $env:HOMEPATH/Develop/gitlab/NSD/p_terraform
 }
 
-function vagrant_up {
-    vagrant up 
-}
-
-function vagrant_status {
-    vagrant status
-}
-
+# Vagrant shortcut functions
 function vagrant_destroy {
-    vagrant destroy
+    vagrant destroy $args
 }
-
-function vagrant_suspend {
-    vagrant suspend
-}
-
 function vagrant_resume {
-    vagrant resume
+    vagrant resume $args
+}
+function vagrant_status {
+    vagrant status $args
+}
+function vagrant_suspend {
+    vagrant suspend $args
+}
+function vagrant_up {
+    vagrant up $args
 }
 
+# Alias Definitions
 Set-Alias -Name less -Value Get-Content
+
 Set-Alias -Name cdg -Value cd_git
 Set-Alias -Name cdgl -Value cd_gitlab
 Set-Alias -Name cdv -Value cd_vagrant
 Set-Alias -Name cdtf -Value cd_p_terraform
-Set-Alias -Name vup -Value vagrant_up
-Set-Alias -Name vstat -Value vagrant_status
-Set-Alias -Name vdes -Value vagrant_destroy
-Set-Alias -Name vsus -Value vagrant_suspend
-Set-Alias -Name vres -Value vagrant_resume
 
+Set-Alias -Name vdes -Value vagrant_destroy
+Set-Alias -Name vres -Value vagrant_resume
+Set-Alias -Name vstat -Value vagrant_status
+Set-Alias -Name vsus -Value vagrant_suspend
+Set-Alias -Name vup -Value vagrant_up
+
+# Modifying the Prompt with Posh-Git
 Import-Module -Name posh-git
+
+# Adding SSH Key to SSH Agent
 Start-SshAgent
 Add-SshKey $env:USERPROFILE\.ssh\id_rsa.ppk
 
@@ -59,23 +61,91 @@ function Test-Administrator {
     (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-$GitPromptSettings.WorkingForegroundColor = 'Red'
-#$GitPromptSettings.UntrackedForegroundColor = 'Red'
-$GitPromptSettings.DefaultForegroundColor = 'Red'
-$GitPromptSettings.BranchForegroundColor = 'Red'
-$GitPromptSettings.BranchAheadStatusForegroundColor = 'Red'
-$GitPromptSettings.LocalDefaultStatusForegroundColor = 'Red'
-$GitPromptSettings.AfterForegroundColor = 'White'
-$GitPromptSettings.BeforeForegroundColor = 'White'
-$GitPromptSettings.BeforeIndexForegroundColor = 'Green'
-$GitPromptSettings.BranchIdenticalStatusToForegroundColor = 'Cyan'
-$GitPromptSettings.DelimForegroundColor = 'Yellow'
-$GitPromptSettings.EnableFileStatus = 'False'
-$GitPromptSettings.IndexForegroundColor = 'Green'
-$GitPromptSettings.LocalDefaultStatusForegroundColor = 'Green'
-$GitPromptSettings.LocalStagedStatusForegroundColor = 'Yellow'
-$GitPromptSettings.LocalWorkingStatusForegroundColor = 'Magenta'
-$GitPromptSettings.EnableWindowTitle = "~"
+# Git Prompt Settings
+$GitPromptSettings.DefaultForegroundColor                    = 'Red'
+#$GitPromptSettings.BeforeText                                = '['
+$GitPromptSettings.BeforeForegroundColor                     = 'White'
+#$GitPromptSettings.BeforeBackgroundColor                     = ''
+#$GitPromptSettings.DelimText                                 = '|'
+$GitPromptSettings.DelimForegroundColor                      = 'Yellow'
+#$GitPromptSettings.DelimBackgroundColor                      = ''
+#$GitPromptSettings.AfterText                                 = ']'
+$GitPromptSettings.AfterForegroundColor                      = 'White'
+#$GitPromptSettings.AfterBackgroundColor                      = ''
+#$GitPromptSettings.FileAddedText                             = '+'
+#$GitPromptSettings.FileModifiedText                          = '~'
+#$GitPromptSettings.FileRemovedText                           = '-'
+#$GitPromptSettings.FileConflictedText                        = '!'
+#$GitPromptSettings.LocalDefaultStatusSymbol                  = ''
+$GitPromptSettings.LocalDefaultStatusForegroundColor         = 'Green'
+$GitPromptSettings.LocalDefaultStatusForegroundBrightColor   = 'Green'
+#$GitPromptSettings.LocalDefaultStatusBackgroundColor         = ''
+#$GitPromptSettings.LocalWorkingStatusSymbol                  = '!'
+$GitPromptSettings.LocalWorkingStatusForegroundColor         = 'Magenta'
+$GitPromptSettings.LocalWorkingStatusForegroundBrightColor   = 'Red'
+#$GitPromptSettings.LocalWorkingStatusBackgroundColor         = ''
+#$GitPromptSettings.LocalStagedStatusSymbol                   = '~'
+$GitPromptSettings.LocalStagedStatusForegroundColor          = 'Yellow'
+#$GitPromptSettings.LocalStagedStatusBackgroundColor          = ''
+#$GitPromptSettings.BranchUntrackedSymbol                     = ''
+$GitPromptSettings.BranchForegroundColor                     = 'Red'
+#$GitPromptSettings.BranchBackgroundColor                     = ''
+#$GitPromptSettings.BranchGoneStatusSymbol                    = '×'
+$GitPromptSettings.BranchGoneStatusForegroundColor           = 'DarkCyan'
+#$GitPromptSettings.BranchGoneStatusBackgroundColor           = ''
+#$GitPromptSettings.BranchIdenticalStatusToSymbol             = '≡'
+$GitPromptSettings.BranchIdenticalStatusToForegroundColor    = 'Cyan'
+#$GitPromptSettings.BranchIdenticalStatusToBackgroundColor    = ''
+#$GitPromptSettings.BranchAheadStatusSymbol                   = '↑'
+$GitPromptSettings.BranchAheadStatusForegroundColor          = 'Red'
+#$GitPromptSettings.BranchAheadStatusBackgroundColor          = ''
+#$GitPromptSettings.BranchBehindStatusSymbol                  = '↓'
+$GitPromptSettings.BranchBehindStatusForegroundColor         = 'Red'
+#$GitPromptSettings.BranchBehindStatusBackgroundColor         = ''
+#$GitPromptSettings.BranchBehindAndAheadStatusSymbol          = '↕'
+$GitPromptSettings.BranchBehindAndAheadStatusForegroundColor = 'Yellow'
+#$GitPromptSettings.BranchBehindAndAheadStatusBackgroundColor = ''
+#$GitPromptSettings.BeforeIndexText                           = ''
+$GitPromptSettings.BeforeIndexForegroundColor                = 'Green'
+$GitPromptSettings.BeforeIndexForegroundBrightColor          = 'Green'
+#$GitPromptSettings.BeforeIndexBackgroundColor                = ''
+$GitPromptSettings.IndexForegroundColor                      = 'Green'
+$GitPromptSettings.IndexForegroundBrightColor                = 'Green'
+#$GitPromptSettings.IndexBackgroundColor                      = ''
+$GitPromptSettings.WorkingForegroundColor                    = 'Red'
+$GitPromptSettings.WorkingForegroundBrightColor              = 'Red'
+#$GitPromptSettings.WorkingBackgroundColor                    = ''
+#$GitPromptSettings.EnableStashStatus                         = 'False'
+#$GitPromptSettings.BeforeStashText                           = '('
+#$GitPromptSettings.BeforeStashBackgroundColor                = ''
+$GitPromptSettings.BeforeStashForegroundColor                = 'Red'
+#$GitPromptSettings.AfterStashText                            = ')'
+#$GitPromptSettings.AfterStashBackgroundColor                 = ''
+$GitPromptSettings.AfterStashForegroundColor                 = 'Red'
+#$GitPromptSettings.StashBackgroundColor                      = ''
+$GitPromptSettings.StashForegroundColor                      = 'Red'
+$GitPromptSettings.ErrorForegroundColor                      = 'Red'
+#$GitPromptSettings.ErrorBackgroundColor                      = ''
+#$GitPromptSettings.ShowStatusWhenZero                        = 'True'
+#$GitPromptSettings.UntrackedFilesMode                        = ''
+#$GitPromptSettings.AutoRefreshIndex                          = 'True'
+#$GitPromptSettings.BranchBehindAndAheadDisplay               = 'Full'
+#$GitPromptSettings.EnablePromptStatus                        = 'True'
+#$GitPromptSettings.EnableFileStatus                          = 'False'
+#$GitPromptSettings.EnableFileStatusFromCache                 = 'False'
+#$GitPromptSettings.RepositoriesInWhichToDisableFileStatus    = '{}'
+#$GitPromptSettings.DescribeStyle                             = ''
+$GitPromptSettings.EnableWindowTitle                         = '~'
+#$GitPromptSettings.AdminTitlePrefixText                      = 'Administrator: '
+#$GitPromptSettings.DefaultPromptPrefix                       = ''
+#$GitPromptSettings.DefaultPromptSuffix                       = '$('>' * ($nestedPromptLevel + 1)) '
+#$GitPromptSettings.DefaultPromptDebugSuffix                  = '[DBG]$('>' * ($nestedPromptLevel + 1)) '
+#$GitPromptSettings.DefaultPromptEnableTiming                 = 'False'
+#$GitPromptSettings.DefaultPromptPath                         = '$(Get-PromptPath)'
+#$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory      = 'False'
+#$GitPromptSettings.Debug                                     = 'False'
+#$GitPromptSettings.BranchNameLimit                           = '0'
+#$GitPromptSettings.TruncatedBranchSuffix                     = '...'
 
 function prompt {
     $realLASTEXITCODE = $LASTEXITCODE
@@ -97,12 +167,9 @@ function prompt {
         Write-Host ") " -NoNewline -ForegroundColor DarkGray
     }
 
-    #Write-Host " : " -NoNewline -ForegroundColor DarkGray
     Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\','\\'), "~") -NoNewline -ForegroundColor Blue
-    #Write-Host " : " -NoNewline -ForegroundColor DarkGray
     #Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor Magenta
-    #Write-Host " : " -NoNewline -ForegroundColor DarkGray
-
+ 
     $global:LASTEXITCODE = $realLASTEXITCODE
     Write-VcsStatus
     Write-Host "" -NoNewline
