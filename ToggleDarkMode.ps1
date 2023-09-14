2b9b7ff20f622a248f255524d57ccfff7da2ead8
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2023-09-14 20:02:10 +0200

    chore: force transparency to refresh theme

--- ToggleDarkMode.ps1
+++ ToggleDarkMode.ps1
@@ -1,9 +1,11 @@
 Set-PSDebug -Off
 $SystemUsesLightTheme = Get-ItemPropertyValue -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme
 
+$EnableTransparency = 1
 $EnableLightTheme = 1
 if ($SystemUsesLightTheme){
     $EnableLightTheme = 0
 }
-New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value $EnableLightTheme -Type Dword -Force;
 New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value $EnableLightTheme -Type Dword -Force
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value $EnableLightTheme -Type Dword -Force
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name EnableTransparency -Value $EnableTransparency -Type Dword -Force
